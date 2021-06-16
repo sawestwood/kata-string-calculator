bb6f763a8fa07a2882df754762ffef07c362fd64
Steve Westwood <steve.westwood@hexsaw.org.uk>
2021-06-16 13:28:35 +0100

    Convert tests to Junit5

--- src/test/java/uk/org/hexsaw/kata/StringCalculatorTest.java
+++ src/test/java/uk/org/hexsaw/kata/StringCalculatorTest.java
@@ -2,69 +2,69 @@ package uk.org.hexsaw.kata;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class StringCalculatorTest {
-    
+
     private static StringCalculator stringCalculator = StringCalculator.getInstance();
-    
-    @Before
-    public void context() {        
-    }
 
     @Test
     public void canCreateAStringCalculator() {
         assertThat(stringCalculator, notNullValue());
     }
-    
-    @Test 
+
+    @Test
     public void whenAddWithEmptyStringThenGetZero() {
         assertThat(stringCalculator.add(""), equalTo(0));
     }
-    
+
     @Test
     public void whenAddWithSingleNumberThenGetTheNumberSupplied() {
         assertThat(stringCalculator.add("1"), equalTo(1));
     }
- 
+
     @Test
     public void canPerformAdditionWithTwoNumbersCommaSeperated() {
         assertThat(stringCalculator.add("1,2"), equalTo(3));
-    }                                                                      
-    
+    }
+
     @Test
     public void canPerformAdditionWithLotsOfNumbersCommaSeperated() {
         assertThat(stringCalculator.add("1,2,8,12,6"), equalTo(29));
-    }    
+    }
 
     @Test
     public void canPerformAdditionWithNumbersInStringWithANewLine() {
         assertThat(stringCalculator.add("1\n2,3"), equalTo(6));
-    } 
-    
+    }
+
     @Test
     public void canPerformAdditionWithNumbersInStringWithMultipleNewLines() {
         assertThat(stringCalculator.add("9\n2\n3"), equalTo(14));
-    } 
-    
-    @Test(expected=Exception.class)
+    }
+
+    @Test
     public void ensureExceptionRaisedWhenDelimiterNewLineAndCommaCombined() {
-       stringCalculator.add("1\n,3");
-    }  
-    
-    @Test(expected=Exception.class)
+        Exception exception = Assertions.assertThrows(Exception.class, () -> {
+            stringCalculator.add("1\n,3");
+        });
+    }
+
+    @Test
     public void ensureExceptionRaisedWhenDelimiterCommaAndNewLineCombined() {
-       stringCalculator.add("1,\n3");
-    } 
-                                             
+        Exception exception = Assertions.assertThrows(Exception.class, () -> {
+            stringCalculator.add("1,\n3");
+        });
+    }
+
     @Test
     public void canHandleAlternativeDelimiterSpecifiedOnFirstLine() {
         assertThat(stringCalculator.add("//:\n1:2:6"), equalTo(9));
     }
-    
+
     @Test
     public void canHandleAlternativeDelimiterSpecifiedOnFirstLineWithMultipleLines() {
         assertThat(stringCalculator.add("//:\n1:2:6\n5:2"), equalTo(16));
